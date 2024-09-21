c5cef4612abcb2798138001db28cca587a64e225
Viorel Strogoteanu <55164639+ViorelsS@users.noreply.github.com>
2024-09-21 03:49:04 +0200

    Update spinner-class.js

--- assets/js/spinner-class.js
+++ assets/js/spinner-class.js
@@ -1,28 +1,26 @@
 export class Spinner {
     constructor(thickElement, spinner) {
+        this.thicks = [];  // Array per tenere traccia dei thick
         this.createThicks(thickElement, spinner);
-        thickElement.style.setProperty('--initial-rotation', `0deg`);
+        this.rotation = 0;  // Manteniamo una rotazione globale
     }
 
     createThicks(thickElement, spinner) {
         const fragment = document.createDocumentFragment();
         for (let i = 0; i < 35; i++) {
             const clone = thickElement.cloneNode(true);
-            clone.style.setProperty('--initial-rotation', `${(i + 1) * 10}deg`);
+            clone.style.setProperty('--initial-rotation', `${i * 10}deg`);
             fragment.appendChild(clone);
+            this.thicks.push(clone);  // Salviamo i thick nel nostro array
         }
         spinner.appendChild(fragment);
     }
 
-    rotate(thicks, direction) {
+    rotate(direction) {
         const increment = direction === 'add' ? 10 : -10;
-        thicks.forEach((thick) => {
-            const currentRotation =
-                parseFloat(thick.style.getPropertyValue('--rotation')) || 0;
-            thick.style.setProperty(
-                '--rotation',
-                `${currentRotation + increment}deg`
-            );
+        this.rotation += increment;
+        this.thicks.forEach((thick) => {
+            thick.style.transform = `rotate(calc(var(--initial-rotation) + ${this.rotation}deg)) translateY(-25vh)`;
         });
     }
 }
